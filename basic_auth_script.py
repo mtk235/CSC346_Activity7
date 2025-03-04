04a638062094dc0af0961b371e6d03a6e06abb3e
mahir <mahir.kabir03@yahoo.com>
2025-03-04 14:32:59 -0700

    updating basic_auth_script.py to access github API

--- basic_auth_script.py
+++ basic_auth_script.py
@@ -1,12 +1,11 @@
 #! /usr/bin/python3
 
 import requests
-import os
 import passwords
 
 user = passwords.user
 password = passwords.password
 
-r = requests.get("https://httpbin.org/basic-auth/mahir/test", auth=(user, password))
+r = requests.get("https://api.github.com/user/repos", auth=(user, password))
 
 print(r.status_code)
